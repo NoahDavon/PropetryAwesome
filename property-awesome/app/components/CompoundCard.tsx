684d47c0bf9a9d536b1db4cf64b2277ceca67f3a
Mohammed <MohammedE.Fahmy@outlook.com>
2024-04-10 13:28:24 +0200

    Ptr

--- property-awesome/app/components/CompoundCard.tsx
+++ property-awesome/app/components/CompoundCard.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function CompoundCard({property}: Props) {
   return (
-    <div style={{backgroundImage: `url(compoundImages/${property.ID}.png)`}} className={`bg-cover group hover:scale-110 transition-all rounded-xl w-64 lg:w-96 h-64 lg:h-96 relative`}>
+    <div style={{backgroundImage: `url(compoundImages/${property.ID}.png)`}} className={` cursor-pointer bg-cover group hover:scale-110 transition-all rounded-xl w-64 lg:w-96 h-64 lg:h-96 relative`}>
         <Text className=' absolute top-0 left-0 text-black font-semibold group-hover:opacity-0 stroke-black p-4'>{property.Name}</Text>
         <Flex className=' p-4 z-10 bg-white w-full text-orange-300 opacity-0 font-semibold group-hover:opacity-75 transition-all rounded-xl'>
             <Text className=' '>{property.Name}</Text>
