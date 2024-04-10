a21c74aa77f188c6321b9b5231c7472fe878149a
Mohammed <MohammedE.Fahmy@outlook.com>
2024-04-10 19:54:04 +0200

    Install json server

--- property-awesome/app/page.tsx
+++ property-awesome/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import FavoritesContext from "./FavoritesContext";
 import CompoundCard from "./components/CompoundCard";
-import { TabList, Tabs, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
+import { TabList, Tabs, Tab, TabPanels, TabPanel, Text } from "@chakra-ui/react";
 import { FaBuilding, FaHeart } from "react-icons/fa";
 import FavoritesPanel from "./components/FavoritesPanel";
 
@@ -13,7 +13,7 @@ export default function Home() {
       <Tabs className=" text-orange-300" variant={"enclosed"} colorScheme="orange">
         <TabList display={"flex"} justifyContent={"center"} gap={"100px"}>
           <Tab><FaBuilding className="mx-4" size={"30px"}/></Tab>
-          <Tab><FaHeart className="mx-4" size={"30px"}/></Tab>
+          <Tab><FaHeart className="mx-4" size={"30px"}/><Text>{favorites.length? favorites.length : ""}</Text></Tab>
         </TabList>
         <TabPanels>
           <TabPanel>
