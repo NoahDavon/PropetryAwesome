8f247215dcde43fb00c3f7e74c5a74d6b15110cd
Mohammed <MohammedE.Fahmy@outlook.com>
2024-04-12 14:20:28 +0200

    Maps on listings

--- property-awesome/app/components/CompoundCard.tsx
+++ property-awesome/app/components/CompoundCard.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Property from '../types/Property'
-import { Text, IconButton } from '@chakra-ui/react'
+import { Text, IconButton, Modal, ModalOverlay, CloseButton, useDisclosure, ModalHeader, ModalContent, ModalBody } from '@chakra-ui/react'
 import Rounder from '../functions/Rounder'
 import { FaMapMarkerAlt } from 'react-icons/fa'
 import Link from 'next/link'
 import FavoriteButton from './FavoriteButton'
-
+import dynamic from 'next/dynamic'
+const Map = dynamic(() => import("@/app/components/MapComponent"), {
+    ssr: false
+})
 type Props = {
     property: Property
 }
@@ -19,16 +22,32 @@ export default function CompoundCard({property}: Props) {
             <CompoundDetails property={property}/>
         </div>
         <div className='absolute bottom-0 left-0 lg:hidden flex w-full opacity-90 p-2 text-orange-300 rounded-xl bg-white '><CompoundDetails property={property}/></div>
-    </div></Link>
+    </div>
+    </Link>
   )
 }
 
 function CompoundDetails({property}: Props){
+    const {isOpen, onClose, onOpen} = useDisclosure();
     return (
         <div className='flex justify-between flex-grow items-center font-semibold text-orange-300'>
+            <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay/>
+            
+            <ModalContent>
+            <ModalHeader>
+            <CloseButton onClick={onClose}/>
+            </ModalHeader>
+                <ModalBody>
+                    <div className='w-full h-[300px]'>
+                    <Map position={[property.Long, property.Lat]} Name={property.Name}/>
+                    </div>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
             <Text>{`£${Rounder(property["Price (Villa)"])}`}</Text>
             <Text>{`£${Rounder(property["Price/Meter"])} /m`}<sup>2</sup></Text>
-            <IconButton variant={"unstyled"} className=' hover:scale-110 transition-transform' aria-label='location' fontSize={"30px"} icon={<FaMapMarkerAlt/>}/>
+            <IconButton variant={"unstyled"} className=' hover:scale-110 transition-transform' aria-label='location' fontSize={"30px"} icon={<FaMapMarkerAlt/>} onClick={e => {e.preventDefault(); onOpen()}}/>
             <FavoriteButton id={property.id}/>
         </div>
     )
