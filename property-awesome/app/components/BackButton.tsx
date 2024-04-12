48e1d677473c6471ea28d49c82fe9fbc5299b9c1
Mohammed <MohammedE.Fahmy@outlook.com>
2024-04-12 11:44:41 +0200

    Back button

--- property-awesome/app/components/BackButton.tsx
+++ property-awesome/app/components/BackButton.tsx
@@ -1,12 +1,13 @@
 import { IconButton } from '@chakra-ui/react'
 import Link from 'next/link'
 import React from 'react'
-import { BiLeftArrow } from 'react-icons/bi'
+import { FaArrowLeft } from 'react-icons/fa6'
+
 
 type Props = {}
 
 export default function BackButton({}: Props) {
   return (
-    <Link href={"/"}><div className=' sticky text-orange-300'><IconButton aria-label='Back to home' fontSize={"30px"} icon={<BiLeftArrow/>}/></div></Link>
+    <Link href={"/"}><div className=' sticky text-orange-300'><IconButton aria-label='Back to home' fontSize={"30px"} icon={<FaArrowLeft/>}/></div></Link>
   )
 }
