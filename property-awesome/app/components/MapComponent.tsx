558efbc8ef43fd65e785e186506bc27d394d5428
Mohammed <MohammedE.Fahmy@outlook.com>
2024-04-12 13:16:10 +0200

    Adjusting zoom

--- property-awesome/app/components/MapComponent.tsx
+++ property-awesome/app/components/MapComponent.tsx
@@ -14,7 +14,7 @@ type Props = {
 
 export const MapComponent = ({position, Name}: Props) => {
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={position} zoom={12} scrollWheelZoom={false}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
