87864dae87460a396d825505c55f83d9b59874d9
Mohammed <MohammedE.Fahmy@outlook.com>
2024-04-12 14:39:56 +0200

    Marker shenanigans

--- property-awesome/app/components/MapComponent.tsx
+++ property-awesome/app/components/MapComponent.tsx
@@ -1,12 +1,20 @@
 'use client'
-import { LatLngExpression } from 'leaflet'
+import L, { LatLngExpression } from 'leaflet'
 import React, { useEffect, useState } from 'react'
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { Marker } from 'react-leaflet/Marker'
 import { Popup } from 'react-leaflet/Popup'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import 'leaflet/dist/leaflet.css';
+let DefaultIcon = L.icon({
+    iconUrl: 'imgs/marker.png',
+    shadowUrl: 'leaflet/dist/images/marker-shadow.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -20]
+});
 
+L.Marker.prototype.options.icon = DefaultIcon;
 type Props = {
     position: LatLngExpression,
     Name: string
